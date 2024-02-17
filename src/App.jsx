4675950eac748f6359065c3963f222f6e9bfc61a
kastastin <kostya242421@gmail.com>
2024-02-17 18:10:38 +0200

    App layout (padding)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
-export default function App() {
-	return <h1 className="text-3xl font-bold underline">Initial setup</h1>;
-}
+const App = () => (
+	<main className="relative">
+		Nav
+
+		<section className="xl:padding-l wide:padding-r padding-b">
+			Hero
+		</section>
+
+		<section className="padding">
+			PopularProducts
+		</section>
+
+		<section className="padding">
+			SuperQuality
+		</section>
+
+		<section className="padding-x py-10">
+			Services
+		</section>
+
+		<section className="padding">
+			SpecialOffer
+		</section>
+
+		<section className="padding bg-pale-blue">
+			CustomerReviews
+		</section>
+
+		<section className="w-full padding-x sm:py-32 py-16">
+			Subscribe
+		</section>
+
+		<section className="padding-x padding-t pb-8 bg-black">
+			Footer
+		</section>
+	</main>
+);
+
+export default App;
